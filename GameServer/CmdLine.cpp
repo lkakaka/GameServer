211f3fe397bf63b2200b0ee96ef5e9c3e32b6d3c
liuj <liujan_dev@163.com>
2020-07-23 14:44:10 +0800

    屏蔽linux下的cmdline(一直会读到一个空字符串)

--- GameServer/CmdLine.cpp
+++ GameServer/CmdLine.cpp
@@ -6,21 +6,28 @@
 
 static std::thread cmd_thread;
 
+
+void handleCmd(std::string& cmd) {
+	PyGILState_STATE py_state = PyGILState_Ensure();
+	PyObject* cmdObj = PyUnicode_FromString(cmd.c_str());
+	if (cmdObj != NULL) {
+		PyObject* args = PyTuple_New(1);
+		PyTuple_SetItem(args, 0, cmdObj);
+		callPyFunction("cmd_line.CmdLine", "on_cmd", args);
+	}
+	PyGILState_Release(py_state);
+}
+
 void waitInput() {
 	std::string cmd;
 	while (true) {
 		std::cin >> cmd;
-		PyGILState_STATE py_state = PyGILState_Ensure();
-		PyObject* cmdObj = PyUnicode_FromString(cmd.c_str());
-		if (cmdObj != NULL) {
-			PyObject* args = PyTuple_New(1);
-			PyTuple_SetItem(args, 0, cmdObj);
-			callPyFunction("cmd_line.CmdLine", "on_cmd", args);
-		}
-		PyGILState_Release(py_state);
+		handleCmd(cmd);
 	}
 }
 
 void startCmd() {
+#ifdef WIN32	// linux下一直会读到空字符(未知原因)
 	cmd_thread = std::thread(waitInput);
+#endif
 }
